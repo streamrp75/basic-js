4ec8f3eb0346f3de69f110e06d3c15684adc3243
streamrp75 <79273733+streamrp75@users.noreply.github.com>
2021-03-24 01:16:47 +0300

    feat: add dream-team code

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,6 +1,20 @@
 const CustomError = require("../extensions/custom-error");
-
-module.exports = function createDreamTeam(/* members */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
-};
+module.exports = function createDreamTeam(matrix) {
+  let name ="";
+  if (matrix==null || typeof(matrix) != "object" || matrix[0]==null ){
+    return false;
+  }else{
+  matrix.forEach((elem) => {
+    if (typeof(elem) == "string")
+    {
+      elem=elem.trim()
+      name=name+elem[0].toUpperCase()
+    }
+  });
+  if(name.length == 0 ){
+    return false;
+  }else{
+    return name.split("").sort().join("")
+  }
+  }
+}
